678d58ba1150459c0f144f329b4dc5d24b15e4fa
Ganesh Pandey <pandey.ganesh12@gmail.com>
2022-12-01 23:55:48 +0530

    Solve Day 1

--- 2022/Day1_1.py
+++ 2022/Day1_1.py
@@ -1,1 +1,14 @@
-tes11
+with open("input.txt") as file:
+    contents = file.readlines()
+
+currentCalories = 0
+maxCalories = 0
+
+for content in contents:
+    if (content == "\n"):
+        maxCalories = max(currentCalories, maxCalories)
+        currentCalories = 0
+    else:
+        currentCalories += int(content)
+
+print(maxCalories)
